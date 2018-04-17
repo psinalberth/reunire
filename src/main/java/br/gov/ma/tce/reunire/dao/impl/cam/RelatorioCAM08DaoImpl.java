3c080fa0ed7731dd3299b119a8c409d75b2cc423
Josemar Mendes Leal Neto <josemarmendes17@gmail.com>
2018-04-17 11:32:02 -0300

    CAM 008 - DEMONSTRATIVO No 10: UNIDADES DE ENSINO (U.E.)

---
+++ src/main/java/br/gov/ma/tce/reunire/dao/impl/cam/RelatorioCAM08DaoImpl.java
@@ -0,0 +1,71 @@
+package br.gov.ma.tce.reunire.dao.impl.cam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import br.gov.ma.tce.reunire.dao.DemonstrativoDao;
+import br.gov.ma.tce.reunire.dao.impl.PrestacaoDaoImpl;
+import br.gov.ma.tce.reunire.model.vo.cam.RelatorioCAM08VO;
+import br.gov.ma.tce.reunire.model.vo.gestor.UnidadeVO;
+
+@Stateless
+public class RelatorioCAM08DaoImpl extends PrestacaoDaoImpl<RelatorioCAM08VO> implements DemonstrativoDao<RelatorioCAM08VO> {
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<RelatorioCAM08VO> recuperaDados(Map<String, Object> params) {
+		
+		List<UnidadeVO> listaUnidades = recuperarUnidades(params);
+		List<Integer> listaIdsUnidades = extrairIds(listaUnidades);
+		
+		String sql = 
+				
+		"select unidade_id, unidade_ensino, endereco, " + 
+		"	vaga_pre_escola, vaga_fundamental, vaga_medio, " + 
+		"	quantidade_sala, quantidade_laboratorio, quantidade_biblioteca, quantidade_quadra " + 
+		"from prestacao.cam08 cam " +
+		"   where cam.unidade_id in (:unidades) and " +
+		"	((:modulo is null) or (cam.modulo_id = :modulo)) ";
+		
+		List<Object[]> rows = entityManager.createNativeQuery(sql)
+				.setParameter("unidades", listaIdsUnidades)
+				.setParameter("modulo", 1)
+				.getResultList();
+		
+		List<RelatorioCAM08VO> dados = new ArrayList<RelatorioCAM08VO>(rows.size());
+		
+		params.put("exercicio", 2017);
+		
+		for (Object [] row : rows) {
+			
+			RelatorioCAM08VO dado = new RelatorioCAM08VO();
+			
+			Optional<UnidadeVO> unidade = listaUnidades.stream().filter(u -> u.getId().equals(Integer.parseInt(String.valueOf(row[0])))).findFirst();
+			
+			dado.setIdUnidade(Integer.parseInt(String.valueOf(row[0])));
+			dado.setDescricaoUnidade(unidade != null ? unidade.get().getNome().toUpperCase() : "");
+			dado.setUnidadeEnsino(String.valueOf(row[1]));
+			dado.setEndereco(String.valueOf(row[2]));
+			dado.setCapacidadeAlunosPreEscolar(Integer.parseInt(String.valueOf(row[3])));
+			dado.setCapacidadeAlunosFundamental(Integer.parseInt(String.valueOf(row[4])));
+			dado.setCapacidadeAlunosEnsinoMedio(Integer.parseInt(String.valueOf(row[5])));
+			dado.setNumeroSalaDeAula(Integer.parseInt(String.valueOf(row[6])));
+			dado.setNumeroLaboratorio(Integer.parseInt(String.valueOf(row[7])));
+			dado.setNumeroBiblioteca(Integer.parseInt(String.valueOf(row[8])));
+			dado.setNumeroQuadraEsporte(Integer.parseInt(String.valueOf(row[9])));
+			
+			dados.add(dado);
+		}
+		
+		return dados;
+	}
+	
+	@Override
+	public String getNomeRelatorio() {
+		return "relatoriocam08.jasper";
+	}
+}
---
+++ src/main/java/br/gov/ma/tce/reunire/model/vo/cam/RelatorioCAM08VO.java
@@ -0,0 +1,73 @@
+package br.gov.ma.tce.reunire.model.vo.cam;
+
+import br.gov.ma.tce.reunire.model.vo.generic.DemonstrativoVO;
+
+public class RelatorioCAM08VO extends DemonstrativoVO {
+	
+	private String unidadeEnsino;
+	private String endereco;
+	private Integer capacidadeAlunosPreEscolar;
+	private Integer capacidadeAlunosFundamental;
+	private Integer capacidadeAlunosEnsinoMedio;
+	private Integer numeroSalaDeAula;
+	private Integer numeroLaboratorio;
+	private Integer numeroBiblioteca;
+	private Integer numeroQuadraEsporte;
+	
+	public String getUnidadeEnsino() {
+		return unidadeEnsino;
+	}
+	public void setUnidadeEnsino(String unidadeEnsino) {
+		this.unidadeEnsino = unidadeEnsino;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public Integer getCapacidadeAlunosPreEscolar() {
+		return capacidadeAlunosPreEscolar;
+	}
+	public void setCapacidadeAlunosPreEscolar(Integer capacidadeAlunosPreEscolar) {
+		this.capacidadeAlunosPreEscolar = capacidadeAlunosPreEscolar;
+	}
+	public Integer getCapacidadeAlunosFundamental() {
+		return capacidadeAlunosFundamental;
+	}
+	public void setCapacidadeAlunosFundamental(Integer capacidadeAlunosFundamental) {
+		this.capacidadeAlunosFundamental = capacidadeAlunosFundamental;
+	}
+	public Integer getCapacidadeAlunosEnsinoMedio() {
+		return capacidadeAlunosEnsinoMedio;
+	}
+	public void setCapacidadeAlunosEnsinoMedio(Integer capacidadeAlunosEnsinoMedio) {
+		this.capacidadeAlunosEnsinoMedio = capacidadeAlunosEnsinoMedio;
+	}
+	public Integer getNumeroSalaDeAula() {
+		return numeroSalaDeAula;
+	}
+	public void setNumeroSalaDeAula(Integer numeroSalaDeAula) {
+		this.numeroSalaDeAula = numeroSalaDeAula;
+	}
+	public Integer getNumeroLaboratorio() {
+		return numeroLaboratorio;
+	}
+	public void setNumeroLaboratorio(Integer numeroLaboratorio) {
+		this.numeroLaboratorio = numeroLaboratorio;
+	}
+	public Integer getNumeroBiblioteca() {
+		return numeroBiblioteca;
+	}
+	public void setNumeroBiblioteca(Integer numeroBiblioteca) {
+		this.numeroBiblioteca = numeroBiblioteca;
+	}
+	public Integer getNumeroQuadraEsporte() {
+		return numeroQuadraEsporte;
+	}
+	public void setNumeroQuadraEsporte(Integer numeroQuadraEsporte) {
+		this.numeroQuadraEsporte = numeroQuadraEsporte;
+	}
+	
+	
+}
