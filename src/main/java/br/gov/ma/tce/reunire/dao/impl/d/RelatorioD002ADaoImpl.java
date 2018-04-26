c8ae027ff0c068dd500bbe5dc2a347e89a065adb
saeti18 <saeti18@psinalberth>
2018-04-26 10:09:26 -0300

    Correção do relatório D002A

--- src/main/java/br/gov/ma/tce/reunire/dao/impl/d/RelatorioD002ADaoImpl.java
+++ src/main/java/br/gov/ma/tce/reunire/dao/impl/d/RelatorioD002ADaoImpl.java
@@ -1,12 +1,12 @@
 package br.gov.ma.tce.reunire.dao.impl.d;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
 import javax.ejb.Stateless;
+import javax.persistence.NoResultException;
 
 import br.gov.ma.tce.reunire.dao.DemonstrativoDao;
 import br.gov.ma.tce.reunire.dao.impl.PrestacaoDaoImpl;
@@ -27,13 +27,29 @@ public class RelatorioD002ADaoImpl extends PrestacaoDaoImpl<RelatorioD002AVO> im
 		String sql = 
 				
 		"select " + 
-		"	d.unidade_id, " + 
-		"	cr.codigo || '.0.0.0.00.00' cod_cat, cr.descricao desc_cat,  " + 
-		"	cr.codigo || '.'|| o.codigo || '.0.0.00.00' cod_ori, o.descricao desc_ori,  " + 
-		"	cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.0.00.00' cod_esp, e.descricao desc_esp,  " + 
-		"	cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.' || r.codigo || '.00.00' cod_rub, r.descricao desc_rub,   " + 
-		"	cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.' || r.codigo || '.' || to_char(a.codigo, 'FM00') || '.00' cod_ali, a.descricao desc_ali,  " + 
-		"	d.valor_atual " + 
+		"	d.unidade_id, " +
+		"	(case " +
+		"		when cr.id_categoria_ec_receita is not null then  cr.codigo || '.0.0.0.00.00' " +
+		"		else substring(d.nr, 1, 1) || '.0.0.0.00.00' " +
+		"	end) cod_cat, cr.descricao desc_cat, " +
+		"	(case  " +
+		"		when o.id_origem is not null then cr.codigo || '.'|| o.codigo || '.0.0.00.00' " +
+		"		else substring(d.nr, 1, 1) || '.' || substring(d.nr, 2, 1) || '.0.0.00.00' " +
+		"	end) cod_ori,  " +
+		"	o.descricao desc_ori, " +
+		"	(case  " +
+		"		when e.id_especie is not null then cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.0.00.00' " +
+		"		else substring(d.nr, 1, 1) || '.' || substring(d.nr, 2, 1) || '.' || substring(d.nr, 3, 1) || '.0.00.00' " +
+		"	end) cod_esp, e.descricao desc_esp,  " +
+		"	(case  " +
+		"		when r.id_rubrica is not null then cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.' || r.codigo || '.00.00' " +
+		"		else substring(d.nr, 1, 1) || '.' || substring(d.nr, 2, 1) || '.' || substring(d.nr, 3, 1) || '.' || substring(d.nr, 4, 1) || '.00.00' " +
+		"	end) cod_rub, r.descricao desc_rub,   " +
+		"	(case  " +
+		"		when a.id_alinea is not null then cr.codigo || '.'|| o.codigo || '.' ||  e.codigo || '.' || r.codigo || '.' || to_char(a.codigo, 'FM00') || '.00' " +
+		"		else substring(d.nr, 1, 1) || '.' || substring(d.nr, 2, 1) || '.' || substring(d.nr, 3, 1) || '.' || substring(d.nr, 4, 1) || '.' || to_char(cast(substring(d.nr, 5, 2) as integer), 'FM00') || '.00' " +
+		"	end) cod_ali, a.descricao desc_ali, " +
+		"	d.valor_atual, nr.id_natureza_receita " +
 		"from ( " + 
 		"	select " + 
 		"		unidade_id, substring(regexp_replace(natureza_receita, '[.]', '', 'g'), 1, 6) || '00' nr, descricao,  " + 
@@ -46,6 +62,7 @@ public class RelatorioD002ADaoImpl extends PrestacaoDaoImpl<RelatorioD002AVO> im
 		"		prestacao.d002a " + 
 		"	where " + 
 		"		unidade_id in (:unidades) and " + 
+		"       valor_atual <> 0 and " +
 		"		((:modulo is null) or (modulo_id = :modulo)) " + 
 		"	group by " + 
 		"		unidade_id, substring(regexp_replace(natureza_receita, '[.]', '', 'g'), 1, 6), descricao " + 
@@ -63,11 +80,10 @@ public class RelatorioD002ADaoImpl extends PrestacaoDaoImpl<RelatorioD002AVO> im
 		"order by " + 
 		"	cod_cat, cod_ori, cod_esp, cod_rub, cod_ali ";
 		
-		List<RelatorioD002AVO> listaVo = new ArrayList<>();
+		List<RelatorioD002AVO> dados = new ArrayList<>();
 		
-		List<UnidadeVO> listaUnidadeVO = recuperarUnidades(params);
-		List<Integer> listaIdsUnidades = extrairIds(listaUnidadeVO);
-		
+		List<UnidadeVO> listaUnidades = recuperarUnidades(params);
+		List<Integer> listaIdsUnidades = extrairIds(listaUnidades);
 				
 		@SuppressWarnings("unchecked")
 		List<Object[]> lista = entityManager.createNativeQuery(sql)
@@ -75,28 +91,105 @@ public class RelatorioD002ADaoImpl extends PrestacaoDaoImpl<RelatorioD002AVO> im
 				.setParameter("modulo", Integer.valueOf(String.valueOf(params.get("modulo"))))
 				.getResultList();
 		
-		for(Object[] l : lista) {
+		for(Object[] row : lista) {
 			
-			RelatorioD002AVO relatorio = new RelatorioD002AVO();
+			RelatorioD002AVO dado = new RelatorioD002AVO();
 			
-			Optional<UnidadeVO> unidade = listaUnidadeVO.stream().filter(u -> u.getId().equals(Integer.parseInt(String.valueOf(l[0])))).findFirst();
+			Optional<UnidadeVO> unidade = listaUnidades.stream().filter(u -> u.getId().equals(Integer.parseInt(String.valueOf(row[0])))).findFirst();
 			
-			relatorio.setIdUnidade(Integer.parseInt(String.valueOf(l[0])));
-			relatorio.setDescricaoUnidade(unidade != null ? unidade.get().getNome().toUpperCase() : "");
-			relatorio.setCodigoCategoriaEconomica(l[1].toString());
-			relatorio.setDescricaoCategoriaEconomica(l[2].toString());
-			relatorio.setCodigoOrigem(l[3].toString());
-			relatorio.setDescricaoOrigem(l[4].toString());
-			relatorio.setCodigoEspecie(l[5].toString());
-			relatorio.setDescricaoEspecie(l[6].toString());
-			relatorio.setCodigoRubrica(l[7].toString());
-			relatorio.setDescricaoRubrica(l[8].toString());
-			relatorio.setCodigoAlinea(l[9].toString());
-			relatorio.setDescricaoAlinea(l[10].toString());
-			relatorio.setValor(new BigDecimal((new Double(l[11].toString()))));
-			listaVo.add(relatorio);
+			dado.setIdUnidade(Integer.parseInt(String.valueOf(row[0])));
+			dado.setDescricaoUnidade(unidade != null ? unidade.get().getNome().toUpperCase() : "");
+			dado.setCodigoCategoriaEconomica(row[1].toString());
+			dado.setCodigoOrigem(row[3].toString());
+			dado.setCodigoEspecie(row[5].toString());
+			dado.setCodigoRubrica(row[7].toString());
+			dado.setCodigoAlinea(row[9].toString());
+			dado.setValor(toBigDecimal(row[11]));
+			
+			if (row[12] == null) {
+				
+				try {
+					
+					Object categoria = entityManager.createNativeQuery("select descricao from sae.vw_natureza_receita where codigo_natureza_receita = :natureza and ativo = 'S'")
+							.setParameter("natureza", dado.getCodigoCategoriaEconomica())
+							.getSingleResult();
+					
+					if (categoria != null) {
+						dado.setDescricaoCategoriaEconomica(String.valueOf(categoria));
+					}
+					
+				} catch (NoResultException ex) {
+					dado.setDescricaoCategoriaEconomica("CLASSIFICAÇÃO DESCONHECIDA");
+				}
+				
+				try {
+					
+					Object origem = entityManager.createNativeQuery("select descricao from sae.vw_natureza_receita where codigo_natureza_receita = :natureza and ativo = 'S'")
+							.setParameter("natureza", dado.getCodigoOrigem())
+							.getSingleResult();
+					
+					if (origem != null) {
+						dado.setDescricaoOrigem(String.valueOf(origem));
+					}
+					
+				} catch (NoResultException ex) {
+					dado.setDescricaoOrigem("CLASSIFICAÇÃO DESCONHECIDA");
+				}
+				
+				try {
+					
+					Object especie = entityManager.createNativeQuery("select descricao from sae.vw_natureza_receita where codigo_natureza_receita = :natureza and ativo = 'S'")
+							.setParameter("natureza", dado.getCodigoEspecie())
+							.getSingleResult();
+					
+					if (especie != null) {
+						dado.setDescricaoEspecie(String.valueOf(especie));
+					}
+					
+				} catch (NoResultException ex) {
+					dado.setDescricaoEspecie("CLASSIFICAÇÃO DESCONHECIDA");
+				}
+				
+				try {
+					
+					Object rubrica = entityManager.createNativeQuery("select descricao from sae.vw_natureza_receita where codigo_natureza_receita = :natureza and ativo = 'S'")
+							.setParameter("natureza", dado.getCodigoRubrica())
+							.getSingleResult();
+					
+					if (rubrica != null) {
+						dado.setDescricaoRubrica(String.valueOf(rubrica));
+					}
+					
+				} catch (NoResultException ex) {
+					dado.setDescricaoRubrica("CLASSIFICAÇÃO DESCONHECIDA");
+				}
+				
+				try {
+					
+					Object alinea = entityManager.createNativeQuery("select descricao from sae.vw_natureza_receita where codigo_natureza_receita = :natureza and ativo = 'S'")
+							.setParameter("natureza", dado.getCodigoAlinea())
+							.getSingleResult();
+					
+					if (alinea != null) {
+						dado.setDescricaoAlinea(String.valueOf(alinea));
+					}
+					
+				} catch (NoResultException ex) {
+					dado.setDescricaoAlinea("CLASSIFICAÇÃO DESCONHECIDA");
+				}
+				
+			} else {
+			
+				dado.setDescricaoCategoriaEconomica(row[2].toString());
+				dado.setDescricaoOrigem(row[4].toString());
+				dado.setDescricaoEspecie(row[6].toString());
+				dado.setDescricaoRubrica(row[8].toString());
+				dado.setDescricaoAlinea(row[10].toString());
+			}
+			
+			dados.add(dado);
 		}
 		
-		return listaVo;
+		return dados;
 	}
 }
