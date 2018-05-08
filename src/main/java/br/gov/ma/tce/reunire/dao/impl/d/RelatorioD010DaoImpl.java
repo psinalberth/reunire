fcd6e46c4d8ce1b5c932ecd09faaef7f2b245d06
psinalberth <psinalberth@github.com>
2018-05-08 09:27:47 -0300

    Criação de relatório D010.

---
+++ src/main/java/br/gov/ma/tce/reunire/dao/impl/d/RelatorioD010DaoImpl.java
@@ -0,0 +1,98 @@
+package br.gov.ma.tce.reunire.dao.impl.d;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import javax.ejb.Stateless;
+
+import br.gov.ma.tce.reunire.dao.DemonstrativoDao;
+import br.gov.ma.tce.reunire.dao.impl.PrestacaoDaoImpl;
+import br.gov.ma.tce.reunire.model.vo.d.RelatorioD010VO;
+import br.gov.ma.tce.reunire.model.vo.gestor.UnidadeVO;
+
+@Stateless
+public class RelatorioD010DaoImpl extends PrestacaoDaoImpl<RelatorioD010VO> implements DemonstrativoDao<RelatorioD010VO>{
+
+	@Override
+	public List<RelatorioD010VO> recuperaDados(Map<String, Object> params) {
+	
+		String sql = 
+			
+		"select " + 
+			"unidade_id,  " + 
+			"regexp_replace(natureza_receita, '[.]', '', 'g') nr,  " + 
+			"(case when nr.id_natureza_receita is not null then nr.descricao else 'CLASSIFICAÇÃO DESCONHECIDA' end) descricao,  " + 
+			"previsao_atualizada, receita_realizada  " +
+		"from  " + 
+			"prestacao.bo01 bo  " +
+		"left join sae.vw_natureza_receita nr on  " + 
+			"(regexp_replace(nr.codigo_natureza_receita, '[.]', '', 'g') = regexp_replace(natureza_receita, '[.]', '', 'g') and nr.ativo = 'S' or  " + 
+			"(regexp_replace(nr.codigo_natureza_receita, '[.]', '', 'g') = regexp_replace(natureza_receita, '[.]', '', 'g') and  " +
+			"nr.id_natureza_receita = (select max(d2.id_natureza_receita) from sae.vw_natureza_receita d2 where nr.codigo_natureza_receita = d2.codigo_natureza_receita and d2.ativo = 'N')))  " +
+		"where  " + 
+			"unidade_id in (:unidades) and  " +
+			"((:modulo is null) or (modulo_id = :modulo)) and  " +
+			"cast(regexp_replace(natureza_receita, '[.]', '', 'g') as integer) > 0  " +
+		"order by  " +
+			"unidade_id, regexp_replace(natureza_receita, '[.]', '', 'g')";
+		
+		List<RelatorioD010VO> dados = new ArrayList<RelatorioD010VO>();
+		
+		List<UnidadeVO> listaUnidades = recuperarUnidades(params);
+		List<Integer> listaIdsUnidades = extrairIds(listaUnidades);
+		
+		@SuppressWarnings("unchecked")
+		List<Object[]> rows = entityManager.createNativeQuery(sql)
+				.setParameter("unidades", listaIdsUnidades)
+				.setParameter("modulo", params.get("modulo"))
+				.getResultList();
+		
+		
+		// Remover todos os itens que possuam descendentes
+		
+		rows.removeIf(new Predicate<Object[]>() {
+
+			@Override
+			public boolean test(Object[] obj) {
+				
+				Pattern pattern = Pattern.compile(".*([1-9]+)");
+				String codigo = String.valueOf(obj[1]);
+				Matcher matcher = pattern.matcher(codigo);
+				
+				matcher.find();
+				
+				return (rows.stream().filter(item -> String.valueOf(item[1]).startsWith(codigo.substring(0, matcher.end())) && Integer.valueOf(String.valueOf(item[1])).compareTo(Integer.valueOf(String.valueOf(obj[1]))) > 0).collect(Collectors.toList())).size() > 0;
+			}
+		});
+		
+		for (Object [] row : rows) {
+		
+			RelatorioD010VO dado = new RelatorioD010VO();
+			
+			Optional<UnidadeVO> unidade = listaUnidades.stream().filter(u -> u.getId().equals(Integer.parseInt(String.valueOf(row[0])))).findFirst();
+			
+			dado.setIdUnidade(Integer.parseInt(String.valueOf(row[0])));
+			dado.setCodigoNaturezaReceita(String.valueOf(row[1]).
+					replaceFirst("(\\d{1})(\\d{1})(\\d{1})(\\d{1})(\\d{2})(\\d{2})", "$1.$2.$3.$4.$5.$6"));
+			dado.setDescricaoUnidade(unidade != null ? unidade.get().getNome().toUpperCase() : "");
+			dado.setDescricao(String.valueOf(row[2]));
+			dado.setValorOrcado(toBigDecimal(row[3]));
+			dado.setValorArrecadado(toBigDecimal(row[4]));
+			
+			dados.add(dado);
+		}
+		
+		return dados;
+	}
+
+	@Override
+	public String getNomeRelatorio() {	
+		return "relatoriod010.jasper";
+	}
+}
---
+++ src/main/java/br/gov/ma/tce/reunire/model/vo/d/RelatorioD010VO.java
@@ -0,0 +1,45 @@
+package br.gov.ma.tce.reunire.model.vo.d;
+
+import java.math.BigDecimal;
+
+import br.gov.ma.tce.reunire.model.vo.generic.DemonstrativoVO;
+
+public class RelatorioD010VO extends DemonstrativoVO {
+	
+	private String codigoNaturezaReceita;
+	private String descricao;
+	private BigDecimal valorOrcado;
+	private BigDecimal valorArrecadado;
+	
+	public String getCodigoNaturezaReceita() {
+		return codigoNaturezaReceita;
+	}
+	
+	public void setCodigoNaturezaReceita(String codigoNaturezaReceita) {
+		this.codigoNaturezaReceita = codigoNaturezaReceita;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public BigDecimal getValorOrcado() {
+		return valorOrcado;
+	}
+	
+	public void setValorOrcado(BigDecimal valorOrcado) {
+		this.valorOrcado = valorOrcado;
+	}
+	
+	public BigDecimal getValorArrecadado() {
+		return valorArrecadado;
+	}
+	
+	public void setValorArrecadado(BigDecimal valorArrecadado) {
+		this.valorArrecadado = valorArrecadado;
+	}
+}
