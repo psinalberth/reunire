a3c3f7788ba42a18eee2636a26e008e6ada25cb7
inalberth <inalberth07@gmail.com>
2018-06-07 06:32:40 -0300

    Inclusão de query para demonstrativo 25 (CAM)

---
+++ src/main/java/br/gov/ma/tce/reunire/dao/impl/cam/RelatorioCAM25DaoImpl.java
@@ -0,0 +1,142 @@
+package br.gov.ma.tce.reunire.dao.impl.cam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import javax.ejb.Stateless;
+
+import br.gov.ma.tce.reunire.dao.DemonstrativoDao;
+import br.gov.ma.tce.reunire.dao.impl.PrestacaoDaoImpl;
+import br.gov.ma.tce.reunire.model.vo.cam.RelatorioCAM25VO;
+import br.gov.ma.tce.reunire.model.vo.gestor.UnidadeVO;
+
+@Stateless
+public class RelatorioCAM25DaoImpl extends PrestacaoDaoImpl<RelatorioCAM25VO> implements DemonstrativoDao<RelatorioCAM25VO> {
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<RelatorioCAM25VO> recuperaDados(Map<String, Object> params) {
+		
+		String sql = 
+				
+		"select " + 
+		"    emp.unidade_id, emp.numero_empenho ne, initcap(mod.nome) modalidade, emp.data_contabilizacao, nd.codigo, " + 
+		"    (case  " + 
+		"        when credor_extraordinario is not null then credor_extraordinario " + 
+		"        when credor_cpf_cnpj is not null then credor_cpf_cnpj " + 
+		"        when credor_unidade_id is not null then cast(credor_unidade_id as text) " + 
+		"    end) credor, emp.valor val_emp, " + 
+		"    coalesce(ref.valor, 0) val_ref, coalesce(anu.valor, 0) val_anu, " + 
+		"    coalesce(liq.valor, 0) val_liq, coalesce(pag.valor, 0) val_pag, " + 
+		"    (case when pag.forma_pagamento_id = 1 then pag.numero_cheque_fisico end) doc, " + 
+		"    (case  " + 
+		"    	when coalesce(pag.valor, 0) > 0 then coalesce(pag.valor, 0) - coalesce(liq.valor, 0) " + 
+		"    	when coalesce(liq.valor, 0) > 0 then coalesce(liq.valor, 0) - coalesce(emp.valor, 0) - coalesce(anu.valor, 0) - coalesce(ref.valor, 0) " + 
+		"    	when coalesce(anu.valor, 0) > 0 then emp.valor + coalesce(ref.valor, 0) - coalesce(anu.valor, 0) " + 
+		"    	when coalesce(ref.valor, 0) > 0 then emp.valor + coalesce(ref.valor, 0)  " + 
+		"    	else emp.valor " + 
+		"    end) saldo " + 
+		"from  " + 
+		"	remessa.empenho emp " + 
+		"left join remessa.modalidade_empenho mod on " + 
+		"	mod.modalidade_empenho_id = emp.modalidade_empenho_id " + 
+		"left join remessa.natureza_despesa nd on  " + 
+		"	nd.natureza_despesa_id = emp.natureza_despesa_id " + 
+		"left join remessa.fonte_recursos fon on " + 
+		"	fon.fonte_recursos_id = emp.fonte_recurso_id " + 
+		"left join ( " + 
+		"    select  " + 
+		"		ref.empenho_id, sum(ref.valor) valor " + 
+		"    from  " + 
+		"		remessa.empenho_reforco ref " + 
+		"    group by " + 
+		"		ref.empenho_id " + 
+		") ref on ref.empenho_id = emp.empenho_id " + 
+		"left join ( " + 
+		"	select  " + 
+		"		anu.empenho_id, sum(anu.valor) valor  " + 
+		"	from  " + 
+		"		remessa.empenho_anulado anu " + 
+		"	group by " + 
+		"		anu.empenho_id " + 
+		") anu on anu.empenho_id = emp.empenho_id " + 
+		"left join ( " + 
+		"	select " + 
+		"		liq.empenho_id, sum(sub.valor) valor " + 
+		"	from  " + 
+		"		remessa.liquidacao liq " + 
+		"	inner join remessa.subelemento_liquidacao sub on " + 
+		"		sub.liquidacao_id = liq.liquidacao_id " + 
+		"	left join remessa.liquidacao_estorno est on " + 
+		"		est.liquidacao_id = liq.liquidacao_id " + 
+		"	where " + 
+		"		est.liquidacao_id is null and liq.empenho_id is not null " + 
+		"	group by " + 
+		"		liq.empenho_id " + 
+		") liq on liq.empenho_id = emp.empenho_id " + 
+		"left join ( " + 
+		"    select  " + 
+		"		liq.empenho_id, pag.forma_pagamento_id, pag.numero_cheque_fisico, sum(coalesce(sub.valor, 0) - coalesce(subdev.valor, 0)) valor " + 
+		"    from  " + 
+		"		remessa.pagamento pag " + 
+		"    inner join remessa.liquidacao liq on " + 
+		"		liq.liquidacao_id = pag.liquidacao_id " + 
+		"    inner join remessa.subelemento_liquidacao sub on " + 
+		"		sub.liquidacao_id = liq.liquidacao_id " + 
+		"    left join remessa.pagamento_estornado est on " + 
+		"		est.pagamento_id = pag.pagamento_id " + 
+		"    left join remessa.pagamento_devolucao dev on " + 
+		"		dev.pagamento_id = pag.pagamento_id " + 
+		"    left join remessa.subelemento_devolucao_pagamento subdev on " + 
+		"		subdev.pagamento_devolucao_id = dev.pagamento_devolucao_id " + 
+		"    where " + 
+		"		est.pagamento_id is null and liq.empenho_id is not null " + 
+		"    group by " + 
+		"		liq.empenho_id, pag.forma_pagamento_id, pag.numero_cheque_fisico " + 
+		") pag on pag.empenho_id = emp.empenho_id " + 
+		"where " + 
+		"	unidade_id in (:unidades) " + 
+		"order by " + 
+		"	unidade_id, data_contabilizacao, numero_empenho";
+		
+		List<UnidadeVO> listaUnidades = recuperarUnidades(params);
+		List<Integer> listaIdsUnidades = extrairIds(listaUnidades);
+		
+		List<Object[]> rows = entityManager.createNativeQuery(sql)
+				.setParameter("unidade", listaIdsUnidades)
+				.getResultList();
+		
+		List<RelatorioCAM25VO> dados = new ArrayList<RelatorioCAM25VO>();
+		
+		for(Object[] row : rows) {
+			
+			RelatorioCAM25VO dado = new RelatorioCAM25VO();
+			
+			Optional<UnidadeVO> unidade = listaUnidades.stream().filter(u -> u.getId().equals(Integer.parseInt(String.valueOf(row[0])))).findFirst();
+			
+			dado.setIdUnidade(Integer.parseInt(String.valueOf(row[0])));
+			dado.setDescricaoUnidade(unidade != null ? unidade.get().getNome().toUpperCase() : "");
+			dado.setNumeroEmpenho(String.valueOf(row[1]));
+			dado.setModalidade(String.valueOf(row[2]));
+			dado.setDataContabil(toDate(row[3]));
+			dado.setNaturezaDespesa(String.valueOf(row[4]));
+			dado.setCredor(row[5] != null ? String.valueOf(row[5]) : "");
+			dado.setValorEmpenhado(toBigDecimal(row[6]));
+			dado.setValorReforcado(toBigDecimal(row[7]));
+			dado.setValorAnulado(toBigDecimal(row[8]));
+			dado.setValorPago(toBigDecimal(row[9]));
+			dado.setSaldo(toBigDecimal(row[11]));
+			
+			dados.add(dado);
+		}
+		
+		return dados;
+	}
+
+	@Override
+	public String getNomeRelatorio() {
+		return "relatoriocam25.jasper";
+	}
+}
---
+++ src/main/java/br/gov/ma/tce/reunire/model/vo/cam/RelatorioCAM25VO.java
@@ -0,0 +1,109 @@
+package br.gov.ma.tce.reunire.model.vo.cam;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import br.gov.ma.tce.reunire.model.vo.generic.DemonstrativoVO;
+
+public class RelatorioCAM25VO extends DemonstrativoVO {
+	
+	private String numeroEmpenho;
+	private String modalidade;
+	private Date dataContabil;
+	private String naturezaDespesa;
+	private String credor;
+	private BigDecimal valorEmpenhado;
+	private BigDecimal valorReforcado;
+	private BigDecimal valorAnulado;
+	private BigDecimal valorLiquidado;
+	private BigDecimal valorPago;
+	private BigDecimal saldo;
+
+	public String getNumeroEmpenho() {
+		return numeroEmpenho;
+	}
+
+	public void setNumeroEmpenho(String numeroEmpenho) {
+		this.numeroEmpenho = numeroEmpenho;
+	}
+
+	public String getModalidade() {
+		return modalidade;
+	}
+
+	public void setModalidade(String modalidade) {
+		this.modalidade = modalidade;
+	}
+
+	public Date getDataContabil() {
+		return dataContabil;
+	}
+
+	public void setDataContabil(Date dataContabil) {
+		this.dataContabil = dataContabil;
+	}
+
+	public String getNaturezaDespesa() {
+		return naturezaDespesa;
+	}
+
+	public void setNaturezaDespesa(String naturezaDespesa) {
+		this.naturezaDespesa = naturezaDespesa;
+	}
+
+	public String getCredor() {
+		return credor;
+	}
+
+	public void setCredor(String credor) {
+		this.credor = credor;
+	}
+
+	public BigDecimal getValorEmpenhado() {
+		return valorEmpenhado;
+	}
+
+	public void setValorEmpenhado(BigDecimal valorEmpenhado) {
+		this.valorEmpenhado = valorEmpenhado;
+	}
+
+	public BigDecimal getValorReforcado() {
+		return valorReforcado;
+	}
+
+	public void setValorReforcado(BigDecimal valorReforcado) {
+		this.valorReforcado = valorReforcado;
+	}
+
+	public BigDecimal getValorAnulado() {
+		return valorAnulado;
+	}
+
+	public void setValorAnulado(BigDecimal valorAnulado) {
+		this.valorAnulado = valorAnulado;
+	}
+
+	public BigDecimal getValorLiquidado() {
+		return valorLiquidado;
+	}
+
+	public void setValorLiquidado(BigDecimal valorLiquidado) {
+		this.valorLiquidado = valorLiquidado;
+	}
+
+	public BigDecimal getValorPago() {
+		return valorPago;
+	}
+
+	public void setValorPago(BigDecimal valorPago) {
+		this.valorPago = valorPago;
+	}
+
+	public BigDecimal getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(BigDecimal saldo) {
+		this.saldo = saldo;
+	}
+}
